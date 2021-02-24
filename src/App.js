23f7b5cb37397a8a8dd05bae2aa7f2bf2bbd2599
LucasLeibs <dr.lucasleibs@gmail.com>
2021-02-23 23:23:44 -0800

    added react snap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from 'react'
 import {BrowserRouter as Router, Switch, Route, Redirect} from "react-router-dom";
 import Nav from './components/Nav';
 import Home from './components/Home';
@@ -8,11 +9,32 @@ import Catalog from './components/Catalog';
 import Contact from './components/Contact';
 import BrowsePieces from './components/BrowsePieces';
 import Main from './containers/Main';
+import ReactDOM from 'react-dom'
+import ScrollSnap from 'scroll-snap'
+
 // import blue from '../images/glass/IMG_7240.JPG'
+
+function callback() {
+  console.log('snapped')
+}
 function App() {
+ const container = React.createRef()
+
+  const bindScrollSnap = () => {
+    const element = container.current
+    const snapElement = new ScrollSnap(element, {
+      snapDestinationY: '90%',
+    })
+
+    snapElement.bind(callback)
+  }
+
+  useEffect(() => {
+   bindScrollSnap()
+  }, [])
   return (
   <Router>
-    <div className="container">
+    <div ref={container} className="container">
     <Switch>
       <Route exact path="/" component={Main}/>
       <Route exact path="/work" component={BrowsePieces}/>
