6b3eb74d6b96270e5c49271edd68feacae2b190e
LucasLeibs <dr.lucasleibs@gmail.com>
2021-02-24 15:38:42 -0800

    added slideshow in beginning

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,24 +1,35 @@
-import React from 'react'
-import MailOutlineRoundedIcon from '@material-ui/icons/MailOutlineRounded';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import MailOutlineRoundedIcon from "@material-ui/icons/MailOutlineRounded";
+import Button from "@material-ui/core/Button";
+import emailjs from "emailjs-com";
 export default function Contact() {
-    return (
-        <section id="contact">
-      
-        {/* <h1>INQUIRE ABOUT A PIECE</h1>
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm("service_qmwlage", "template_ipmt7qr", e.target, "user_yPPl4aKf5yAqWiC6gZq8t")
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+      e.target.reset()
+  };
+
+  return (
+    <section id="contact">
+      {/* <h1>INQUIRE ABOUT A PIECE</h1>
         <Button variant="contained" >
        <MailOutlineRoundedIcon style={{ fontSize: 40 }}></MailOutlineRoundedIcon>
        </Button> */}
-<form id="form" class="topBefore">
-		
-        <input id="name" type="text" placeholder="NAME"/>
-        <input id="email" type="text" placeholder="E-MAIL"/>
-        <textarea id="message" type="text" placeholder="MESSAGE"></textarea>
-<input id="submit" type="submit" value="GO!"/>
-
-</form>
-
-       </section>
-    
-    )
+      <form onSubmit={(e) => sendEmail(e)} id="form" class="topBefore">
+        <input id="name" type="text" placeholder="NAME" name="name" />
+        <input id="email" type="text" placeholder="E-MAIL" name="email" />
+        <textarea id="message" type="text" placeholder="MESSAGE" name="message"></textarea>
+        <input id="submit" type="submit" value="GO!" />
+      </form>
+    </section>
+  );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,52 @@
 import React from 'react'
-import lantern from '../images/glass/lantern2jpg.jpg'
+
 import logo from '../images/glass/logo.png'
 import blue from '../images/glass/IMG_7240.JPG'
 import red from '../images/glass/IMG_7219.JPG'
 import blue2 from '../images/glass/IMG_7248.JPG'
+import lantern from '../images/glass/IMG_7171.JPG'
+import pea from '../images/glass/IMG_7188.JPG'
+import 'react-slideshow-image/dist/styles.css'
+import { Slide } from 'react-slideshow-image';
+const slideImages = [
+    lantern,
+    blue,
+    red,
+    pea,
+    blue2,
+  ];
+  
 export default function Home() {
+ 
     return (
         <section id="home">
-             <div>
-        <h1>LEIBERMAN GLASS</h1>
-         <img className="lantern" src={blue}></img>
            
+        <h1>LEIBERMAN GLASS</h1>
+        
+         <div className="slide-container">
+        <Slide>
+          <div className="each-slide">
+            <div >
+             <img src={slideImages[0]}></img>
             </div>
-            
+          </div>
+          <div className="each-slide">
+          <div>
+             <img src={slideImages[1]}></img>
+            </div>
+          </div>
+          <div className="each-slide">
+          <div>
+             <img src={slideImages[2]}></img>
+            </div>
+          </div>
+          <div className="each-slide">
+          <div>
+             <img src={slideImages[3]}></img>
+            </div>
+          </div>
+        </Slide>
+      </div>
       
        
 
@@ -20,6 +54,6 @@ export default function Home() {
       
         
        
-        </section>
+     </section>
     )
 }
